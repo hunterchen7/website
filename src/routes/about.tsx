2b1c89d7fa761ab4f631d141372a8c6339935393
Hunter Chen <hello@hunterchen.ca>
2025-07-29 17:16:55 -0400

    Update about.tsx

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -4,12 +4,12 @@ import Geneial from "~/components/logos/Geneial";
 
 export default function About() {
   return (
-    <main class="text-center p-4 mx-auto my-auto justify-center flex flex-col font-mono text-violet-200 pb-20 h-screen max-w-[47rem] text-sm md:text-base gap-8">
+    <main class="text-center p-4 mx-auto my-auto justify-center flex flex-col font-mono text-violet-200 pb-20 md:h-screen md:max-w-[47rem] text-sm md:text-base gap-8">
       <Title>About Me</Title>
       <h1 class="text-2xl sm:text-4xl font-thin leading-tight mx-auto max-w-[14rem] md:max-w-none">
         about me..
       </h1>
-      <p class="mx-auto leading-snug text-base">
+      <p class="mx-auto leading-snug">
         I was born in Taiwan 🇹🇼, and grew up in Calgary, Canada 🇨🇦. I&apos;m a
         computer science student @{" "}
         <Link href="https://www.uwo.ca" external>
